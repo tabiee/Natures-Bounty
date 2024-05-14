708de043f3ce086d11e4a9b22eb7b210fb588f22
Aapo Vanhainen <89449819+ab0136@users.noreply.github.com>
2024-05-14 10:16:23 +0300

    deleted unnecessary from input system

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,18 +1,59 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyController : Enemy
 {
-    // Start is called before the first frame update
+    [SerializeField] private float lookRadius = 10f;  // Detection range
+    [SerializeField] private float attackRadius = 5f; // Attack range
+    [SerializeField] private float patrolTime = 5f;   // Time between patrols
+
+    Transform target;  // The player
+    NavMeshAgent agent; // Navmesh agent
+    bool isPatrolling = false;
+
     void Start()
     {
-        
+        target = targetPosition.transform;
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Patrol());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            if (distance <= attackRadius)
+            {
+                // Attack the target
+                // Add your attack code here
+            }
+        }
+    }
+
+    IEnumerator Patrol()
+    {
+        while (true)
+        {
+            if (!isPatrolling)
+            {
+                Vector3 randomDirection = Random.insideUnitSphere * lookRadius;
+                randomDirection += transform.position;
+                NavMeshHit hit;
+                NavMesh.SamplePosition(randomDirection, out hit, lookRadius, 1);
+                Vector3 finalPosition = hit.position;
+
+                agent.SetDestination(finalPosition);
+                isPatrolling = true;
+            }
+
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                isPatrolling = false;
+
+            yield return new WaitForSeconds(patrolTime);
+        }
     }
 }
